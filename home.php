c1b518aa79ce1a25018d5935a9fcfe99dea7de3c
Shakil Ahmed Avro <84856581+sa2avro@users.noreply.github.com>
2021-10-02 15:18:10 +0600

    Update home.php

--- home.php
+++ home.php
@@ -18,7 +18,7 @@ if(!isset($_SESSION['userName'])){
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>CoutDown Lance Website</title>
-    <?php include "css/style.css" ?>
+    <?php include "style.css" ?>
 </head>
 
 <body>
